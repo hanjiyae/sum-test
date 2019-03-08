6db6461b39be39113c25bd0a6a40f8d245a33a78
hanjiyae <gksch98@naver.com>
2019-03-08 00:32:19 -0800

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "sum.h"
+
+main()
+{
+	printf("Selcet two number : ");
+	scanf("%d %d", &a &b);
+
+	int result = sum(int a, int b);
+	
+	printf("result = %d\n", result);	
+
+	return sum;
+
+}
+
+
+
